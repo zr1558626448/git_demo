be798f6ee394aabbaedb870a14b3298c18d025b2
1558626448@qq.com <1558626448@qq.com>
2022-07-30 10:07:50 +0800

    hot-fix commit

--- git/src/main/java/com/zhang/git/TestGit.java
+++ git/src/main/java/com/zhang/git/TestGit.java
@@ -9,5 +9,6 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Hello World!");
+        System.out.println("Hello World! ---- hot-fix分支创建的");
     }
 }
